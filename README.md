b6ecf298f56e86342b0c740e16424b27c2b5068c
Johnny919 <johnnyyik5744@gmail.com>
2023-08-18 12:20:15 +0800

    ha

--- README.md
+++ README.md
@@ -5,9 +5,8 @@ Data Series Classification
 ## Steps 
 1. ./data_preprocess: Preprocess time series and return train_dataloader, test_window, and labels on testing data
 2. ./ts_model & ./train: Initiate Time Series Classification Model and Train using train_dataloader
-3. ./explanation: Using the trained model to generate dCAM on the testing dataset.
-4. ./anomaly_detection: detect the anomaly and generate a predicted dataset
-5. ./metrics: Provide visualisation and metrics calculation
+3. ./explanation & ./detection: Using the trained model to generate dCAM on the testing dataset, and detecting anomaly using STD
+4. ./metrics: Provide visualisation and metrics calculation
 
 
 ## Data
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from train import *
 from data_preprocess import anomaly_generation, stock_gen
 from classtrainer import InceptionTime
 from metrics.score_calculation import evaluate_metrics
+from detection import anomaly
 from sklearn.model_selection import train_test_split
 '''
     # ------------- The logic is like that ----------- #
@@ -17,99 +18,30 @@ from sklearn.model_selection import train_test_split
     4. On the adjusted DCAM matrix, we make anomaly detection applied on both single sequence
 '''
 
-def dcam_result(model, inputs, last_conv_layer, fc_layer, device = 'cpu'):
-    '''
-    
-    args:
-        inputs - shape (B x S x D)
 
-    '''
-    
-    window, seq_len, dim = inputs.shape
-    DCAM_m = DCAM(model, 'cpu', last_conv_layer = last_conv_layer, fc_layer = fc_layer)
-
-    total_len = window + seq_len 
-    result = np.zeros((total_len - seq_len, total_len, dim))
-
-    # Create DCAM Matrix for each instance and append to result array
-    for i in range(window):
-        dcam, label = DCAM_m.run(
-            instance = inputs[i, :, :],
-            nb_permutation = 200,
-        )
-        result[i, :, i: i + seq_len] = dcam
-
-    # 2. Based on the result array, take average for each of sliding window
-    final_result = np.mean(result, axis = 0)      
-
-    # 3. Given the first S and last S scaler is only has single value, we scale it larger
-    for num in reversed(range(1, seq_len + 1)):
-        final_result[:][num] = final_result[:][num] * num
-        final_result[:][-num] = final_result[:][-num] * num
-
-    return final_result
-
-def anamoly_detection_one(final_result):
-    '''
-        Using Ideas from TAD-GAN in self-supervised learning 
-        args:
-            final_result: shape (S x D)
-
-    '''
-    # For each timestep, combine all dimension into one to determine anomaly
-    seq_value = np.mean(final_result, axis = 1)
-
-    # Sequence length for ones - or may use max in future
-    avg_seq_value = np.mean(final_result, axis = 1)
-    std_seq_value = np.std(final_result, axis = 1)
-
-    # Set the thresholds
-    threshold = avg_seq_value + 3 * std_seq_value
-
-    index = [1 for idx, value in enumerate(final_result) if value > threshold else 0]
-    
-    return index
 
 
 def main(config):
     
     task_to_solve = config['task']
     input_path = config['path']
+
+    # Step 1: Preprocess the data and generate necessary data for further computation
+
+    dataloader, test_windows, labels = anomaly_generation.preprocess_data('C:/Users/johnn/cam_time_series_new/inputs/train.csv', 300, 0.3, 0.1)
     
-    # Need to split input data into testing and training dataset
-    class1, class2, original_window = preprocess_data('C:/Users/johnn/cam_time_series_new/inputs/train.csv', 300, 0.3, 0.1)
-    
-  
-    
-    # Set up dataloader for training data
-
-    label = [1]* total_length + [0] * total_length
-    label = np.array(label)
-
-    all_class = class1_train + class2_train
-  
-    from torch.utils.data import TensorDataset, DataLoader
-    data_train = torch.from_numpy(all_class) 
-    label_train = torch.from_numpy(label)
-    train_ds = TensorDataset(data_train, label_train)
-    train_loader = DataLoader(train_ds, batch_size= 32)
-
-
-    # --------------------------# 
-
-    dataloader, test_windows, labels = anomaly_generation(...)
-    # Train the data with anomaly class and normal class
+    # Step 2: Initialize a model and train the model.
     model = InceptionTime()
     model = classifcation.train(model, dataloader)
-
-    # Randomly choose an instance
     last_conv_layer = model._modules['layer3']
     fc_layer = model._modules['fc1']
     
-    dcam = dcam_result(model, test_windows, last_conv_layer, fc_layer)
     
-    # Create labels for comparing evaluating metrics
-    pred_label = anamoly_detection_one(dcam)
+    # Step 3: Generate aggragated dcam and predicted anomaly labels using STD
+    dcam = anomaly.dcam_result(model, test_windows, last_conv_layer, fc_layer)
+    pred_label = anomaly.anamoly_detection_one(dcam)
+
+    # Step 4: Evaluate the predicted labels and test labels
     test_label = np.array(labels)
     metrics = evaluate_metrics(pred_label, test_label)
 
